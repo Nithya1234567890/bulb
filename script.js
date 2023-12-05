250e633b9659a70be54f53973eb82d18444b155a
Nithya1234567890 <141919044+Nithya1234567890@users.noreply.github.com>
2023-12-05 23:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var light=document.getElementById('light');
+document.getElementById('on').onclick=function(){
+    light.style.display="block";
+}
+document.getElementById('off').onclick=function(){
+    light.style.display="none";
+}
